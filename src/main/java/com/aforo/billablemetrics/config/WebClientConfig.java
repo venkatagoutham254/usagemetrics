e220a4a714f6baf63cadabf0809fb01d7fb144b7
VaishnavJayaraj <vaishu4112002@gmail.com>
2025-10-12 11:42:13 +0530

    Coming back to old branch which was implemented before staus labels

--- src/main/java/com/aforo/billablemetrics/config/WebClientConfig.java
+++ src/main/java/com/aforo/billablemetrics/config/WebClientConfig.java
@@ -10,7 +10,7 @@ public class WebClientConfig {
     @Bean
     public WebClient productServiceWebClient() {
         return WebClient.builder()
-                .baseUrl("http://54.238.204.246:8080/api/products")
+                .baseUrl("http://54.238.204.246:8080/api/products") // Product client
                 .build();
     }
 
